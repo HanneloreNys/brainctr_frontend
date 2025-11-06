d63f5c82d0b0a0f690bafb6019bc06b186aa8db2
“Hannelore” <Hannelore.nys@telenet.be>
2025-11-06 02:44:18 +0100

    Hardcode API_BASE_URL in script.js voor Netlify fix

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL =
+  "https://brainctrjsnodebackend-production.up.railway.app/api";
 console.log("De API_BASE_URL in script.js is:", API_BASE_URL); //
 
 // Module data (zal dynamisch geladen worden)
